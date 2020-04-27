b61b5819d21345acd0824782ac62471b2c20bf45
Ewen <ewen.herve.35550@gmail.com>
2020-04-27 22:51:01 +0200

    project init

---
+++ connexionDonnées.php
@@ -0,0 +1,29 @@
+<DOCTYPE! html lang="en">
+    <head>
+        <title>Titre</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"><!--A revoir cette balise-->
+        <link rel="icon" type="image/png" href="Img/Favicon/icon.png" /><!--icon dans l'onglet-->
+        <link rel="stylesheet" href="CSS/Style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body class="fuck">
+    
+        <div class="btne">
+            <form action="secret.php" method="post">
+                <p>
+                    <label for="pseudo">Pseudo<input type="password" name="pseudo" id="pseudo"></label><br/>
+                    <label for="mot_de_passe">Mot de passe<input type="password" name="mot_de_passe" id="mot_de_passe"></label><br/>
+                    <input type="submit" value="Valider" />
+                </p>
+            </form> 
+        </div>
+
+        <!--Partie Script-->
+
+        <script src="https://use.fontawesome.com/releases/v5.11.2/js/all.js" data-auto-a11y="true"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+</html>
